535478d2cab160394af927cb58f8130333f1a20d
cursemaker <cursedsena@gmail.com>
2025-02-19 11:26:29 +0700

    2nd fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import { MainContent } from "./components/main.content";
 import { NavBar } from "./components/navbar";
 import { Members } from "./components/members.content";
